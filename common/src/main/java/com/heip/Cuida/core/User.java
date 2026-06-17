608ef251051fd000bd24fccca99816ec0540db7a
hoangmaituanhiep <hoangmaituanhiep@gmail.com>
2026-06-17 14:48:32 +0700

    Update: Add core class and update gitignore

---
+++ common/src/main/java/com/heip/Cuida/core/User.java
@@ -0,0 +1,38 @@
+package com.heip.Cuida.core;
+
+public class User {
+  private String id;
+  private String username;
+  private String email;
+  private String phone;
+  private String birthdate;
+  private String avatarUrl;
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getId() {
+    return id;
+  }
+  public String getUsername() {
+    return username;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public String getPhone() {
+    return phone;
+  }
+  public String getBirthdate() {
+    return birthdate;
+  }
+  public String getAvatarUrl() {
+    return avatarUrl;
+  }
+  public User(String username, String email, String phone, String birthdate, String avatarUrl) {
+    this.username = username;
+    this.email = email;
+    this.phone = phone;
+    this.birthdate = birthdate;
+    this.avatarUrl = avatarUrl;
+  }
+}
